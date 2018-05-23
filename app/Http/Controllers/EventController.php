9fe89ceefd53d94fff91841f816c84d6e37cddd3
jesusrv1103 <jesus21c.jrv@gmail.com>
2018-05-23 14:59:37 -0500

    necesario

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,7 +30,7 @@ class EventController extends Controller
                     // Add color and link on event
                     [
                     'color' => '#f05050',
-                    'url' => 'pass here url and any route',
+                    'url' => 'adquisinet',
                     ]
                     );
             }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,3 +35,11 @@ Route::get('verPartidas/{id}','PartidaController@verPartidas')->name('partidas.v
 Route::get('crearPartidas2/{id}','Partida2Controller@create1')->name('partidas2.create1');
 
 Route::get('event2', 'EventController@index1');
+
+
+Route::get('adquisinet', function()
+{
+	return redirect()->away('http://adquisinet.sazacatecas.gob.mx/index.php
+');
+});
+
